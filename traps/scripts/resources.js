ecc32faf43afd9c72f88abc9bd3cb3a9a8f1f88f
slippyice <slippyice@protonmail.com>
2021-10-17 14:30:13 -0500

    resources :)

--- traps/scripts/resources.js
+++ traps/scripts/resources.js
@@ -6,6 +6,13 @@ images[0] = new Image(); images[0].src = "./assets/images/dev.bmp";
 images[1] = new Image(); images[1].src = "./assets/images/debug_daniel.bmp";
 images[2] = new Image(); images[2].src = "./assets/images/water.bmp";
 images[3] = new Image(); images[3].src = "./assets/images/X.png";
+images[4] = new Image(); images[4].src = "./assets/images/dev_brick.bmp";
+images[5] = new Image(); images[5].src = "./assets/images/ghost_sub.png";
+images[6] = new Image(); images[6].src = "./assets/images/water_night.bmp";
+images[7] = new Image(); images[7].src = "./assets/images/bob_test.png";
+images[8] = new Image(); images[8].src = "./assets/images/dave_test.png";
+images[9] = new Image(); images[9].src = "./assets/images/jimmy_test.png";
+
 
 //create editor
 for (i = 0; i < images.length; i++) {
